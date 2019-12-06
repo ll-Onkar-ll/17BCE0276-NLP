2e63a0ee89280b86eca6cc9933f0c5c7e9c9e193
Onkar Sharma <38857579+ll-Onkar-ll@users.noreply.github.com>
2019-12-06 21:59:24 +0530

    Create Strings.py

---
+++ 06-12-2019/Task-1/Strings.py
@@ -0,0 +1,17 @@
+#printing a simple string
+a = 'This isn\'t the greatest code ever.'
+print(a)
+print("\n")
+
+#printing a paragrapgh
+b =  '''A paragraph is a self-contained unit of a discourse in writing dealing
+with a particular point or idea. A paragraph consists of one or more
+sentences. Though not required by the syntax of any language,
+paragraphs are usually an expected part of formal writing, used to
+organize longer prose.'''
+print(b)
+print("\n")
+
+#printing a string of hindi characters
+c = 'देश के सबसे विश्वसनीय अख़बार पर पढ़ें ताज़ा ख़बरें'
+print(c)
